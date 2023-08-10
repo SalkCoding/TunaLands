6267387b104d0758a2231af28aa6bef4f65e2286
Seungho Baik <sbaik.dev@gmail.com>
2023-08-10 10:28:16 -0400

    remove LandsAPI.getChunkLandType() support for sub servers

--- src/main/kotlin/com/salkcoding/tunalands/lands/LandManager.kt
+++ src/main/kotlin/com/salkcoding/tunalands/lands/LandManager.kt
@@ -443,15 +443,15 @@ class LandManager {
     }
 
     fun onChunkInfoChange(changedChunkQueries: List<String>) {
-        val updates = changedChunkQueries.associateWith {
-            val chunkInfo = landMap[it]
-            if (chunkInfo == null) {
-                null
-            } else {
-                Pair(chunkInfo.worldName, chunkInfo.landType)
-            }
-        }
-        tunaLands.nonMainServerSyncSender.sendChunkInfo(updates)
+//        val updates = changedChunkQueries.associateWith {
+//            val chunkInfo = landMap[it]
+//            if (chunkInfo == null) {
+//                null
+//            } else {
+//                Pair(chunkInfo.worldName, chunkInfo.landType)
+//            }
+//        }
+//        tunaLands.nonMainServerSyncSender.sendChunkInfo(updates)
     }
 
 
